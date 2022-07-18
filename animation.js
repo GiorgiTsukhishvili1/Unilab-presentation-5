4cea2ccb067c4635cc4ee1c51c1300a023f5bc96
Giorgi Tsukhishvili <20100436@ibsu.edu.ge>
2022-07-19 01:02:02 +0400

    Unilab-Presentation-5

---
+++ animation.js
@@ -0,0 +1,7 @@
+// const header = document.querySelector(".header");
+
+// setInterval(() => {
+//   header.style.transform = `translate(${Math.random() * 200}px, ${
+//     Math.random() * 700
+//   }px)`;
+// }, 500);
---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// 1
+
+// const x = [1, 2, 3];
+// const y = [4, 5, 6];
+
+// console.log(x + y);
+
+// 2
+
+// console.log(555555555555555555);
+
+// 3
+
+// console.log(0.1 + 0.2);
+
+// 4
+
+// console.log(50 < 60 < 70);
+
+// console.log(70 > 60 > 50);
+
+// 5
+
+// const data = function () {
+//   console.log(arguments);
+// };
+
+// const data = () => {
+//   console.log(arguments);
+// };
+
+// console.log(data());
+
+// 6
+
+console.log(Math.max());
